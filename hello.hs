89b80732563cca95eeb72e6848db75ffac201d39
zll600 <3400692417@qq.com>
2023-04-29 09:07:47 +0800

    flexible html content

--- hello.hs
+++ hello.hs
@@ -1,3 +1,16 @@
 main = putStrLn myhtml
 
-myhtml = "<html><body>Hello, world!</body></html>"
+html_ content = "<html>" <> content <> "</html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
+
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+myhtml = makeHtml "My page title" "My page content"
