f1f3aae4d2d9ff3a0c59fb47c48cfda1bd1a73d4
zll600 <3400692417@qq.com>
2023-04-29 15:22:55 +0800

    parsing markup part 01

---
+++ Markup.hs
@@ -0,0 +1,16 @@
+module Markup
+  ( Document,
+    Structure (..),
+  )
+where
+
+import Numeric.Natural
+
+type Document = [Structure]
+
+data Structure
+  = Heading Natural String
+  | Paragraph String
+  | UnorderedList [String]
+  | OrderedList [String]
+  | CodeBlock [String]
