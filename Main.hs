efd578a2b1ae40184b3b9b1a603db37169806a17
zll600 <3400692417@qq.com>
2023-05-07 19:23:36 +0800

    working with IO

--- Main.hs
+++ Main.hs
@@ -8,20 +8,28 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main =
+  -- getArgs :: IO [String] Get the program arguments
   getArgs >>= \args ->
     case args of
+      -- No program arguments: reading from stdin and writing to stdout
       [] ->
+        -- getContents IO String Read all of the content from stdin
         getContents >>= \content ->
           putStrLn (process "Empty title" content)
+      -- With input and output file paths as program arguments
       [input, output] ->
+        -- readFile :: FilePath -> IO String Read all of the content from a file
         readFile input >>= \content ->
+          -- doesFileExist :: FilePath -> IO Bool Checks whether a file exists
           doesFileExist output >>= \exists ->
+            -- writeFile :: FilePath -> String -> IO () Write a string into a file
             let writeResult = writeFile output (process input content)
              in if exists
                   then whenIO confirm writeResult
                   else writeResult
+      -- Any other kind of program arguments
       _ ->
-        putStrLn "Usag: runghc Main.hs [-- <input-file> <output-file>]"
+        putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
@@ -29,6 +37,7 @@ process title = Html.render . convert title . Markup.parse
 confirm :: IO Bool
 confirm =
   putStrLn "Are you sure? (y/n)"
+    -- (*>) :: IO a -> IO b -> IO b
     *> getLine
     >>= \answer ->
       case answer of
