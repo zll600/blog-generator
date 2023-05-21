e495f169cea41cdc95db93aa212404dcdeb59376
zll600 <3400692417@qq.com>
2023-05-21 22:08:11 +0800

    feat(options): Add fancy options parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,50 @@
 module Main (main) where
 
 import qualified HsBlog
+import OptParse
+import System.Directory (doesFileExist)
+import System.Exit (exitFailure)
+import System.IO
 
 main :: IO ()
-main = HsBlog.main
+main = do
+  options <- parse
+  case options of
+    ConvertDir input output ->
+      HsBlog.convertDirectory input output
+    ConvertSingle input output -> do
+      (title, inputHandle) <-
+        case input of
+          Stdin ->
+            pure ("", stdin)
+          InputFile file ->
+            (,) file <$> openFile file ReadMode
+
+      outputHandle <-
+        case output of
+          Stdout -> pure stdout
+          OutputFile file -> do
+            exists <- doesFileExist file
+            shouldOpenFile <-
+              if exists
+                then confirm
+                else
+                  pure
+                    True
+            if shouldOpenFile
+              then openFile file WriteMode
+              else exitFailure
+
+      HsBlog.convertSingle title inputHandle outputHandle
+      hClose inputHandle
+      hClose outputHandle
+
+confirm :: IO Bool
+confirm =
+  putStrLn "Are you sure? (y/n)"
+    *> getLine
+    >>= \answer ->
+      case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _ -> putStrLn "Invalid response, use y or n" *> confirm
--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -1,61 +1,24 @@
 -- src/HsBlog.hs
 
 module HsBlog
-  ( main,
+  ( convertSingle,
+    convertDirectory,
     process,
   )
 where
 
 import HsBlog.Convert (convert)
-import qualified HsBlog.Html as Html
-import qualified HsBlog.Markup as Markup
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
+import HsBlog.Html qualified as Html
+import HsBlog.Markup qualified as Markup
+import System.IO
 
-main :: IO ()
-main =
-  -- getArgs :: IO [String] Get the program arguments
-  getArgs >>= \args ->
-    case args of
-      -- No program arguments: reading from stdin and writing to stdout
-      [] ->
-        -- getContents IO String Read all of the content from stdin
-        getContents >>= \content ->
-          putStrLn (process "Empty title" content)
-      -- With input and output file paths as program arguments
-      [input, output] ->
-        -- readFile :: FilePath -> IO String Read all of the content from a file
-        readFile input >>= \content ->
-          -- doesFileExist :: FilePath -> IO Bool Checks whether a file exists
-          doesFileExist output >>= \exists ->
-            -- writeFile :: FilePath -> String -> IO () Write a string into a file
-            let writeResult = writeFile output (process input content)
-             in if exists
-                  then whenIO confirm writeResult
-                  else writeResult
-      -- Any other kind of program arguments
-      _ ->
-        putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+convertSingle :: Html.Title -> Handle -> Handle -> IO ()
+convertSingle title input output = do
+  content <- hGetContents input
+  hPutStrLn output (process title content)
+
+convertDirectory :: FilePath -> String -> IO ()
+convertDirectory = error "Not implemented"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
-
-confirm :: IO Bool
-confirm =
-  putStrLn "Are you sure? (y/n)"
-    -- (*>) :: IO a -> IO b -> IO b
-    *> getLine
-    >>= \answer ->
-      case answer of
-        "y" -> pure True
-        "n" -> pure False
-        _ ->
-          putStrLn "Invalid response. use y or n"
-            *> confirm
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action =
-  cond >>= \result ->
-    if result
-      then action
-      else pure ()
